dc61f923fb34d8b88084dbae92f5d8d1a244447f
linkyh <1428008552@qq.com>
2016-07-06 10:28:12 +0800

    lastest commit

---
+++ ModelPage/js/Login.js
@@ -0,0 +1,15 @@
+//œ‘ æRegister
+function RegisterFormshow() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "";
+}
+//œ‘ ælogin
+function LoginFormshow() {
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "";
+}
+//œ‘ æresearch
+function ResearchFormshow() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("rearchPwdForm").style.display = "";
+}
